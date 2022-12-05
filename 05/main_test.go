3c04040764b5c16a732e103209f54da9b1d4a3df
Martina Simicic <922964+simicic@users.noreply.github.com>
2022-12-05 21:08:42 +0100

    Both parts completed

--- 05/main_test.go
+++ 05/main_test.go
@@ -18,3 +18,20 @@ func TestTopCratesCode(t *testing.T) {
 		}
 	}
 }
+
+var topCrateMover9001Test = []struct {
+	input    string
+	expected string // expected result
+}{
+	{"input_test.txt", "MCD"},
+}
+
+func TestTopCrateMover9001(t *testing.T) {
+	for _, val := range topCrateMover9001Test {
+		got := TopCrateMover9001(val.input)
+
+		if got != val.expected {
+			t.Error("Nah, it was: ", got)
+		}
+	}
+}
