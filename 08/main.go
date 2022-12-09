7b8b53036b5447af9cf9f01506d33fa5132c2365
Martina Simicic <922964+simicic@users.noreply.github.com>
2022-12-09 08:39:09 +0100

    Part 2, scenic scores

--- 08/main.go
+++ 08/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	visibleTrees := VisibleTrees("08/input_test.txt")
+	visibleTrees := VisibleTrees("08/input.txt")
 	fmt.Println("Visible trees: ", visibleTrees)
 }
 
@@ -29,7 +29,7 @@ func visibleTreesInside(inputData [][]int) int {
 	iMax := len(inputData) - 1
 	jMax := len(inputData) - 1
 
-	var scenicScores []int
+	maxScenicScores := 0
 
 	for i := 1; i < iMax; i++ {
 		for j := 1; j < jMax; j++ {
@@ -37,12 +37,14 @@ func visibleTreesInside(inputData [][]int) int {
 			if visible == true {
 				count++
 			}
-			scenicScores = append(scenicScores, scenicScore)
+			if scenicScore > maxScenicScores {
+				maxScenicScores = scenicScore
+			}
 		}
 	}
 
 	// max
-	fmt.Println("Scenic scores: ", scenicScores)
+	fmt.Println("Max scenic score: ", maxScenicScores)
 
 	return count
 }
@@ -57,51 +59,51 @@ func isVisible(inputData [][]int, x int, y int, iMax int, jMax int) (bool, int) 
 	allHigherDown := false
 
 	scenicScoreLeft := 0
-	scenicScorerRight := 0
+	scenicScoreRight := 0
 	scenicScoreUp := 0
 	scenicScoreDown := 0
 
 	// left
 	for j := y - 1; j >= 0; j-- {
+		scenicScoreLeft++
+
 		if inputData[x][j] >= currentTree {
 			allHigherLeft = true
 			break
-		} else {
-			scenicScoreLeft++
 		}
 	}
 
 	// right
 	for j := y + 1; j < iMax+1; j++ {
+		scenicScoreRight++
+
 		if inputData[x][j] >= currentTree {
 			allHigherRight = true
 			break
-		} else {
-			scenicScorerRight++
 		}
 	}
 
 	// up
 	for i := x - 1; i >= 0; i-- {
+		scenicScoreUp++
+
 		if inputData[i][y] >= currentTree {
 			allHigherUp = true
 			break
-		} else {
-			scenicScoreUp++
 		}
 	}
 
 	// down
 	for i := x + 1; i < jMax+1; i++ {
+		scenicScoreDown++
+
 		if inputData[i][y] >= currentTree {
 			allHigherDown = true
 			break
-		} else {
-			scenicScoreDown++
 		}
 	}
 
-	scenicScore = scenicScoreLeft * scenicScorerRight * scenicScoreUp * scenicScoreDown
+	scenicScore = scenicScoreLeft * scenicScoreRight * scenicScoreUp * scenicScoreDown
 
 	if allHigherLeft == false ||
 		allHigherRight == false ||
