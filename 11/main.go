3ec8fff273c7e6efee4a27b1da8ba743953db8e1
Martina Simicic <922964+simicic@users.noreply.github.com>
2022-12-11 13:43:58 +0100

    Working division, broken items value change in map

---
+++ 11/main.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Monkey struct {
+	items               []int
+	inspectedItemsCount int
+	operation           string
+	testDivisionBy      int
+	divisionTrue        int
+	divisionFalse       int
+}
+
+func main() {
+	monkeyBusinessLevel := MonkeyBusinessLevel("11/input_test.txt")
+	fmt.Println("Level of monkey business: ", monkeyBusinessLevel)
+}
+
+func MonkeyBusinessLevel(fileName string) int {
+	monkeyBusinessLevel := 1
+	roundCount := 1
+
+	monkeyBusiness := readInputData(fileName)
+
+	for roundCount <= 20 {
+		for monkeyIndex := 0; monkeyIndex < len(monkeyBusiness); monkeyIndex++ {
+			monkey := monkeyBusiness[monkeyIndex]
+
+			fmt.Println(" ")
+			fmt.Println("Monkey ", monkeyIndex)
+
+			fmt.Println("Processing monkey: ", monkeyIndex, " in iteration count: ", roundCount)
+			if roundCount > 20 {
+				break
+			}
+
+			if len(monkey.items) == 0 {
+				continue
+			}
+
+			for _, item := range monkey.items {
+
+				//Monkey inspects an item with a worry level of 79.
+				//
+				//Monkey gets bored with item. Worry level is divided by 3 to 500.
+				//Current worry level is not divisible by 23.
+				//Item with worry level 500 is thrown to monkey 3.
+
+				fmt.Println("Monkey inspects an item with a worry level of ", item)
+
+				// Worry level is multiplied by 19 to 1501.
+				newValue := processOperation(monkey.operation, item)
+				// Monkey gets bored with item. Worry level is divided by 3 to 500.
+
+				newValue = newValue / 3
+				fmt.Println("Monkey gets bored with item. Worry level is divided by 3 : ", newValue)
+				// Current worry level is not divisible by 23.
+
+				var otherMonkey Monkey
+				var otherMonkeyIndex int
+
+				if newValue%monkey.testDivisionBy == 0 {
+					fmt.Println("Current worry level is not divisible by ", monkey.testDivisionBy)
+					otherMonkeyIndex = monkey.divisionTrue
+					otherMonkey = monkeyBusiness[otherMonkeyIndex]
+				} else {
+					fmt.Println("Current worry level is divisible by ", monkey.testDivisionBy)
+					otherMonkeyIndex = monkey.divisionFalse
+					otherMonkey = monkeyBusiness[otherMonkeyIndex]
+				}
+
+				fmt.Println("Item with worry level ", newValue, " is thrown to monkey ", otherMonkeyIndex)
+
+				otherMonkey.items = append(otherMonkey.items, newValue)
+
+				delete(monkeyBusiness, otherMonkeyIndex)
+				monkeyBusiness[otherMonkeyIndex] = otherMonkey
+
+				(&monkey).inspectedItemsCount = (&monkey).inspectedItemsCount + 1
+			}
+
+			delete(monkeyBusiness, monkeyIndex)
+			monkeyBusiness[monkeyIndex] = monkey
+
+			(&monkey).items = []int{}
+
+			roundCount++
+
+			if roundCount > 20 {
+				break
+			}
+			//printCountsPerMoney(monkeyBusiness)
+		}
+	}
+	return monkeyBusinessLevel
+}
+
+func printCountsPerMoney(monkeyBusiness map[int]Monkey) {
+	for i, v := range monkeyBusiness {
+		fmt.Println("Monkey: ", i, " : ", v.items)
+	}
+}
+
+func processOperation(operation string, item int) int {
+	operationRegexp := regexp.MustCompile(`old (.) (.+)`)
+	operationSlice := operationRegexp.FindStringSubmatch(operation)
+
+	newValue := 0
+	by := 0
+
+	if operationSlice[2] == "old" {
+		by = item
+	} else {
+		by, _ = strconv.Atoi(operationSlice[2])
+	}
+
+	if operationSlice[1] == "*" {
+		newValue = item * by
+	} else {
+		newValue = item + by
+	}
+
+	fmt.Println("Worry level is multiplied by ", by, " to ", newValue)
+
+	return newValue
+}
+
+func readInputData(fileName string) map[int]Monkey {
+	inputData := make(map[int]Monkey)
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.HasPrefix(line, "Monkey ") {
+			// Monkey 0:
+			monkeyRegexp := regexp.MustCompile(`Monkey (.+):`)
+			code := monkeyRegexp.FindStringSubmatch(line)[1]
+			codeInt, _ := strconv.Atoi(code)
+
+			// Starting items: 79, 98
+			scanner.Scan()
+			startingItems := scanner.Text()
+			startingItems = strings.Replace(startingItems, " Starting items: ", "", 1)
+			itemsSlice := strings.Split(startingItems, ",")
+
+			var items []int
+			for _, v := range itemsSlice {
+				vInt, _ := strconv.Atoi(strings.Trim(v, " "))
+				items = append(items, vInt)
+			}
+
+			// Operation: new = old * 19
+			scanner.Scan()
+			operation := scanner.Text()
+			operation = strings.Replace(operation, "  Operation: new = ", "", 1)
+
+			// Test: divisible by 23
+			scanner.Scan()
+			divisibleBy := scanner.Text()
+			divisibleBy = strings.Replace(divisibleBy, " Test: divisible by ", "", 1)
+			divisibleByInt, _ := strconv.Atoi(strings.Trim(divisibleBy, " "))
+
+			// If true: throw to monkey 2
+			scanner.Scan()
+			ifTrue := scanner.Text()
+			ifTrue = strings.Replace(ifTrue, "    If true: throw to monkey ", "", 1)
+			ifTrueInt, _ := strconv.Atoi(strings.Trim(ifTrue, " "))
+
+			// If false: throw to monkey 3
+			scanner.Scan()
+			ifFalse := scanner.Text()
+			ifFalse = strings.Replace(ifFalse, "    If false: throw to monkey ", "", 1)
+			ifFalseInt, _ := strconv.Atoi(strings.Trim(ifFalse, " "))
+
+			monkey := Monkey{
+				items:          items,
+				operation:      operation,
+				testDivisionBy: divisibleByInt,
+				divisionTrue:   ifTrueInt,
+				divisionFalse:  ifFalseInt,
+			}
+
+			inputData[codeInt] = monkey
+		}
+	}
+
+	return inputData
+}
