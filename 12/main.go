299720888215ae25af06e83e97378576c5ff2037
Martina Simicic <922964+simicic@users.noreply.github.com>
2022-12-13 19:29:44 +0100

    Progress till now, not working

--- 12/main.go
+++ 12/main.go
@@ -16,6 +16,7 @@ type Node struct {
 	coordinate        Coordinate
 	value             int
 	distanceFromStart int
+	visited           bool
 }
 
 type GridInfo struct {
@@ -34,14 +35,124 @@ func ShortestPath(fileName string) int {
 	gridInfo := getGridInfo(fileName)
 	gridData := readInputData(fileName, gridInfo)
 
-	fmt.Println(gridInfo)
-	fmt.Println(gridData)
+	startNode := gridData[gridInfo.startNodeAt.toString()]
+	startNode.visited = true
+	delete(gridData, startNode.coordinate.toString())
+	gridData[startNode.coordinate.toString()] = startNode
+
+	gridData = calculateDistances(gridData, gridInfo, startNode)
+
+	printGridData(gridData, gridInfo)
 
 	return 0
 }
 
-func readInputData(fileName string, gridInfo GridInfo) [][]Node {
-	inputData := make([][]Node, gridInfo.sizeX)
+func printGridData(gridData map[string]Node, info GridInfo) {
+	for y := 0; y < info.sizeY; y++ {
+		var line []string
+		for x := 0; x < info.sizeX; x++ {
+			coordinate := Coordinate{x, y}.toString()
+			v := "."
+			if gridData[coordinate].visited == true {
+				v = ">"
+			}
+			line = append(line, string(v))
+		}
+		fmt.Println(line)
+	}
+}
+
+func calculateDistances(gridData map[string]Node, gridInfo GridInfo, node Node) map[string]Node {
+	toCheck := nextToCheck(node, gridInfo)
+
+	for len(toCheck) > 0 {
+		var afterThat []Coordinate
+
+		for _, v := range toCheck {
+			tmp := gridData[v.toString()]
+			if tmp.visited == true && gridInfo.startNodeAt.toString() != tmp.coordinate.toString() {
+				continue
+			}
+
+			if tmp.value <= node.value+1 {
+				fmt.Println("Comparing values: ", tmp.value, node.value)
+				tmp.distanceFromStart = node.distanceFromStart + 1
+				tmp.visited = true
+				delete(gridData, v.toString())
+				gridData[v.toString()] = tmp
+
+				if tmp.coordinate.toString() != gridInfo.endNodeAt.toString() {
+					afterThat = append(afterThat, nextToCheck(tmp, gridInfo)...)
+					fmt.Println(afterThat, " are neighbours of: ", tmp)
+				}
+				node = tmp
+			}
+		}
+
+		var filtered []Coordinate
+		for _, v := range afterThat {
+			if gridData[v.toString()].visited == false {
+				filtered = append(filtered, v)
+			}
+		}
+
+		toCheck = filtered
+		fmt.Println("Neighbours to check: ", toCheck)
+	}
+
+	return gridData
+}
+
+func nextToCheck(node Node, gridInfo GridInfo) []Coordinate {
+	var toCheck []Coordinate
+
+	up, upE := nodeCoordinate(node, "up", gridInfo)
+	if upE != "Error" {
+		toCheck = append(toCheck, up)
+	}
+
+	down, downE := nodeCoordinate(node, "down", gridInfo)
+	if downE != "Error" {
+		toCheck = append(toCheck, down)
+	}
+
+	left, leftE := nodeCoordinate(node, "left", gridInfo)
+	if leftE != "Error" {
+		toCheck = append(toCheck, left)
+	}
+
+	right, rightE := nodeCoordinate(node, "right", gridInfo)
+	if rightE != "Error" {
+		toCheck = append(toCheck, right)
+	}
+
+	return toCheck
+}
+
+func nodeCoordinate(node Node, direction string, gridInfo GridInfo) (Coordinate, string) {
+	switch direction {
+	case "up":
+		if node.coordinate.y+1 < gridInfo.sizeY {
+			return Coordinate{node.coordinate.x, node.coordinate.y + 1}, ""
+		}
+	case "down":
+		if node.coordinate.y-1 >= 0 {
+			return Coordinate{node.coordinate.x, node.coordinate.y - 1}, ""
+		}
+	case "left":
+		if node.coordinate.x+1 < gridInfo.sizeX {
+			return Coordinate{node.coordinate.x + 1, node.coordinate.y}, ""
+		}
+	case "right":
+		if node.coordinate.x-1 >= 0 {
+			return Coordinate{node.coordinate.x - 1, node.coordinate.y}, ""
+		}
+	}
+	return Coordinate{-1, -1}, "Error"
+}
+
+func readInputData(fileName string, gridInfo GridInfo) map[string]Node {
+	inputData := make(map[string]Node, gridInfo.sizeX*gridInfo.sizeY)
 
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -49,23 +160,31 @@ func readInputData(fileName string, gridInfo GridInfo) [][]Node {
 	}
 	defer file.Close()
 
-	x := 0
+	y := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		rowData := make([]Node, gridInfo.sizeY)
 
-		for y, v := range line {
+		for x, v := range line {
 			coordinate := Coordinate{x, y}
+
+			if string(v) == "S" {
+				v = 97
+			} else if string(v) == "E" {
+				v = 122
+			}
 			node := Node{coordinate: coordinate, value: int(v)}
 
-			rowData[y] = node
+			inputData[coordinate.toString()] = node
 		}
-		inputData[x] = rowData
-		x++
+		y++
 	}
 
 	return inputData
+}
+
+func (receiver Coordinate) toString() string {
+	return fmt.Sprintf("[%v,%v]", receiver.x, receiver.y)
 }
 
 func getGridInfo(fileName string) GridInfo {
@@ -80,12 +199,12 @@ func getGridInfo(fileName string) GridInfo {
 	}
 	defer file.Close()
 
-	x := 0
+	y := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		for y, v := range line {
+		for x, v := range line {
 			coordinate := Coordinate{x, y}
 
 			if string(v) == "E" {
@@ -94,10 +213,10 @@ func getGridInfo(fileName string) GridInfo {
 				startCoordinate = coordinate
 			}
 		}
-		sizeY = len(line)
-		x++
+		sizeX = len(line)
+		y++
 	}
-	sizeX = x
+	sizeY = y
 
 	return GridInfo{sizeX: sizeX, sizeY: sizeY, startNodeAt: startCoordinate, endNodeAt: endCoordinate}
 }
