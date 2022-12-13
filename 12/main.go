066bc42439996c3a0ad2beeb4eb84e0b3625d4ca
Martina Simicic <922964+simicic@users.noreply.github.com>
2022-12-13 14:02:02 +0100

    Prepare proper parsing

--- 12/main.go
+++ 12/main.go
@@ -15,25 +15,33 @@ type Coordinate struct {
 type Node struct {
 	coordinate        Coordinate
 	value             int
-	start             bool
-	end               bool
 	distanceFromStart int
 }
 
+type GridInfo struct {
+	startNodeAt Coordinate
+	endNodeAt   Coordinate
+	sizeX       int
+	sizeY       int
+}
+
 func main() {
-	shortestPath := ShortestPath("12/input.txt")
+	shortestPath := ShortestPath("12/input_test.txt")
 	fmt.Println("Shortest path: ", shortestPath)
 }
 
 func ShortestPath(fileName string) int {
-	gridData := readInputData(fileName)
+	gridInfo := getGridInfo(fileName)
+	gridData := readInputData(fileName, gridInfo)
+
+	fmt.Println(gridInfo)
 	fmt.Println(gridData)
 
 	return 0
 }
 
-func readInputData(fileName string) map[string]Node {
-	inputData := make(map[string]Node)
+func readInputData(fileName string, gridInfo GridInfo) [][]Node {
+	inputData := make([][]Node, gridInfo.sizeX)
 
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -45,24 +53,51 @@ func readInputData(fileName string) map[string]Node {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
+		rowData := make([]Node, gridInfo.sizeY)
+
 		for y, v := range line {
 			coordinate := Coordinate{x, y}
-			node := Node{coordinate: coordinate}
+			node := Node{coordinate: coordinate, value: int(v)}
 
-			if string(v) == "E" {
-				node.end = true
-				node.value = int(122)
-			} else if string(v) == "S" {
-				node.start = true
-				node.value = int(97)
-			} else {
-				node.value = int(v)
-			}
-
-			inputData[fmt.Sprintf("[%v,%v]", x, y)] = node
+			rowData[y] = node
 		}
+		inputData[x] = rowData
 		x++
 	}
 
 	return inputData
+}
+
+func getGridInfo(fileName string) GridInfo {
+	sizeX := 0
+	sizeY := 0
+	startCoordinate := Coordinate{}
+	endCoordinate := Coordinate{}
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	x := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		for y, v := range line {
+			coordinate := Coordinate{x, y}
+
+			if string(v) == "E" {
+				endCoordinate = coordinate
+			} else if string(v) == "S" {
+				startCoordinate = coordinate
+			}
+		}
+		sizeY = len(line)
+		x++
+	}
+	sizeX = x
+
+	return GridInfo{sizeX: sizeX, sizeY: sizeY, startNodeAt: startCoordinate, endNodeAt: endCoordinate}
 }
